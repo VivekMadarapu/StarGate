f1489ed827c902e04f51980b8d338c8109625e44
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2021-01-07 11:06:56 -0600

    Create terrain base

---
+++ StarGate/StarGate/StarGate/Terrain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarGate
+{
+    class Terrain
+    {
+        float[,] GenerateWhiteNoise(int width, int height)
+        {
+            Random random = new Random(0); //Seed to 0 for testing
+            float[,] noise = new float[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    noise[i, j] = (float)random.NextDouble() % 1;
+                }
+            }
+
+            return noise;
+        }
+    }
+}
