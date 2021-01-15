5cb3137995dbf0d7cb7906b275a6c9557817d4d9
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2021-01-15 11:06:44 -0600

    lander

--- StarGate/StarGate/StarGate/Game1.cs
+++ StarGate/StarGate/StarGate/Game1.cs
@@ -24,12 +24,10 @@ namespace StarGate
         TitleScreen titleScreen;
         GameScreen gameScreen;
 
-       public static GameState gameState;
-        //just for testing
-        Lander lander;
-
         public static GameState gameState;
-       
+        //just for testing
+        // Lander lander;
+
         //user interface
         GamePadState oldPad;
 
@@ -74,7 +72,7 @@ namespace StarGate
             MousePointer.loadPointerImage(this);
             Button.loadContent(this);
             ship = new spaceShip(Content.Load<Texture2D>("starGateAllSprites"), GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, Content.Load<Texture2D>("projectileTex"));
-            lander = new Lander(Content.Load<Texture2D>("starGateAllSprites"), GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+            // lander = new Lander(Content.Load<Texture2D>("starGateAllSprites"), GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
         }
 
         /// <summary>
@@ -99,7 +97,7 @@ namespace StarGate
             if (newPad.Buttons.Back == ButtonState.Pressed)
                 this.Exit();
             //test lander
-            lander.Update();
+            // lander.Update();
             // TODO: Add your update logic here
             if (gameState == GameState.START_SCREEN) titleScreen.Update(graphics, newPad, oldPad);
 
@@ -129,13 +127,12 @@ namespace StarGate
             else if (gameState == GameState.GAME_SCREEN)
             {
               ship.Draw(spriteBatch);
-              terrain.Draw(spriteBatch);
+              terrain.Draw(spriteBatch, Color.White, 800);
                 //enemy testing
-               
                 ship.Draw(spriteBatch);
                 terrain.Draw(spriteBatch, Color.White, GraphicsDevice.Viewport.Width);
             }
-            lander.Draw(spriteBatch);
+            // lander.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
