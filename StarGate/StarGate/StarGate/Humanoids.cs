abbe89ca3a868b43188c280955707c9fcf11c767
RaghavPu <46982251+RaghavPu@users.noreply.github.com>
2021-01-18 19:42:21 -0600

    Revert "Revert "Humanoids Class""
    This reverts commit 9a4de921876daa49605d42513dd9ec921eafc70e.

---
+++ StarGate/StarGate/StarGate/Humanoids.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace StarGate
+{
+    class Humanoid
+    {
+        public bool caught;
+        public Terrain terrain;
+        int x, y;
+
+        public Humanoid(GraphicsDevice graphics, Terrain terrain)
+        {
+            caught = false;
+            this.terrain = terrain;
+
+            Random random = new Random();
+            int x = random.Next(10, graphics.Viewport.Width - 10);
+            int y = random.Next(10, terrain.terrainContour[x] - 10);
+        }
+    }
+}
